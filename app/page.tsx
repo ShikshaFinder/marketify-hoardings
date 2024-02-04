61832cd732d20f3f2d1fe4d29a8ef29af51b596f
ShikshaFinder <janiharsh794@gmail.com>
2024-02-04 21:40:40 +0530

    harsh

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg text-center justify-center">
-        <h1 className={title()}>Let's &nbsp;</h1>
+        <h1 className={title()}>Let Us &nbsp;</h1>
         <h1 className={title({ color: "violet" })}>digitalize &nbsp;</h1>
         <br />
         <h1 className={title()}>hoarding placing industry</h1>
